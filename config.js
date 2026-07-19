26faa9b6cc3c3ab65d23086aed32739885e889cc
firoozk-f-stack <firooz.kianfar@gmail.com>
2026-07-19 18:02:05 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// 📢 دفترچه ثبت درس‌های آکادمی چیزکد
+// هر وقت درس جدیدی ساختی، فقط بیا خط آخر این لیست یکی مثل بقیه اضافه کن!
+const lessonsConfig = [
+    { title: "متد های str", file: "str-m.html" },
+    { title: "متد های float", file: "float-m.html" },
+    { title: "متد های int", file: "int-m.html" },
+    { title: "متد های boolian", file: "bool-m.html" },
+    { title: "متد های none type", file: "none-m.html" },
+    { title: "متد های list", file: "list-m.html" },
+    { title: "متد های dictionary", file: "dict-m.html" },
+    { title: "متد های tuple", file: "tuple-m.html" },
+    { title: "متد های set" , file: "set-m.html" }
+];
