506d4e51f6796c753f4deff0b5d6c4547f9ca79d
Kianfar-firooz <firooz.kianfar@gmail.com>
2026-07-20 11:49:41 +0330

    Update index-config.js

--- configs/index-config.js
+++ configs/index-config.js
@@ -1,5 +1,5 @@
 const coursesConfig = [
-    { title: "دوره پایتون", url: "python.html" },
-    { title: "Linux cheat sheet", url: "linux.html" }
+    { title: "دوره python", url: "python.html" },
+    { title: "دوره Linux", url: "linux.html" }
     // هر وقت دوره جدیدی ساختی، اینجا اضافه کن
 ];
