05448fafc2a08b6d32b37fe356a50534d743cb0c
Kianfar-firooz <firooz.kianfar@gmail.com>
2026-08-18 15:21:34 +0330

    Update index-config.js

--- configs/index-config.js
+++ configs/index-config.js
@@ -1,9 +1,9 @@
 const coursesConfig = [
-    { title: "دوره python", url: "python.html" },
+    { title: "دوره python", url: "" },
     { title: "دوره Linux", url: "linux.html" },
-    { title: "پاسخ تمارین دوره ها", url: "answers.html" },
-    { title: "پنیر روز | Daily Cheese", url: "challenges.html" },
+    { title: "پاسخ تمارین دوره ها", url: "" },
+    { title: "پنیر روز | Daily Cheese", url: "" },
     { title: "python terminal", url: "terminal-python.html"},
-    { title: "دانلود فایل هایی که لازم میشه", url: "libraries.html" }
+    { title: "دانلود فایل هایی که لازم میشه", url: "" }
     // هر وقت دوره جدیدی ساختی، اینجا اضافه کن
 ];
